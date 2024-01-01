2c7d5d6fe058bc3e5d889086bf51240f75ad05b5
Adeesh Garg <adeesh.g47@gmail.com>
2024-01-02 01:47:04 +0530

    Few UI elements created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>App</div>
+    
+    <div className='h-screen flex justify-center items-center'>
+      <style>
+  @import url('https://fonts.googleapis.com/css2?family=Orbitron&display=swap');
+</style>
+      <div className='h-[400px] w-[500px] border border-solid border-black rounded-md flex justify-center items-center'>
+      <div className='h-[390px] w-[490px] border border-solid border-black rounded-md'>
+      <div className='h-[100px] w-[460px] border border-solid border-black rounded-md mx-auto mt-2 text-right my-auto text-[70px] font-[Orbitron] flex justify-center items-center text-gray-700/25'>
+        8888888
+      </div>
+      </div>
+      </div>
+    </div>
   )
 }
